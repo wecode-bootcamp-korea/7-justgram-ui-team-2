b3c6e327c8d6ead196ce20e6180a087c05cfcc6c
hazel-han99 <tbxl67@naver.com>
2022-10-10 13:19:49 +0900

    mission 5 완료

--- hyeseon/js/main.js
+++ hyeseon/js/main.js
@@ -9,24 +9,59 @@ const commentArray = Array.from(commentList);
 const commentInputArray = Array.from(commentInputList);
 const commentBtnArray = Array.from(commentBtnList);
 
-commentInputArray.forEach(content);
+//업데이트 함수
+function UpdateCommentsDOM(value, comment, writer) {
+  const commentCard = document.createElement("div");
+  commentCard.classList.add("feed-comment-card");
 
+  const accountName = document.createElement("span");
+  accountName.classList.add("highlight");
+  accountName.textContent = writer;
+
+  const commentContent = document.createElement("span");
+  commentContent.textContent = value;
+
+  commentCard.append(accountName, commentContent);
+  comment.append(commentCard);
+}
+
+// 버튼 click 이벤트
 commentBtnArray.forEach((button, index) => {
   button.addEventListener("click", () => {
     const commentInput = commentInputList[index];
     const comment = commentList[index];
 
-    const commentCard = document.createElement("div");
-    commentCard.classList.add("feed-comment-card");
-
-    const accountName = document.createElement("span");
-    accountName.classList.add("highlight");
-    accountName.textContent = "min.young";
-
-    const commentContent = document.createElement("span");
-    commentContent.textContent = commentInput.value;
-
-    commentCard.append(accountName, commentContent);
-    comment.append(commentCard);
+    UpdateCommentsDOM(commentInput.value, comment, "min.young");
+    commentInput.value = "";
   });
 });
+
+// enter이벤트
+commentInputArray.forEach((content, index) => {
+  content.addEventListener("keyup", (e) => {
+    if (e.key === "Enter") {
+      const commentInput = commentInputList[index];
+      const comment = commentList[index];
+
+      UpdateCommentsDOM(commentInput.value, comment, "min.young");
+      commentInput.value = "";
+    }
+  });
+});
+
+function getComments() {
+  fetch("../data/comments.json")
+    .then((res) => res.json())
+    .then((json) => {
+      console.log(json);
+      json.forEach((comments) => {
+        const comment = commentList[comments.Feedindex];
+        UpdateCommentsDOM(
+          comments.commentContent,
+          comment,
+          comments.accountName
+        );
+      });
+    });
+}
+getComments();
